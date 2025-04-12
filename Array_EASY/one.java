e629123a1e7f749bd5e15295b75c21d4b69eb4e6
pkvstar <pk8649726@gmail.com>
2025-04-12 20:31:40 +0530

    second

---
+++ Array_EASY/one.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class one{
+    public static void main(String args[]){
+        int[] arr = {23,56,2,345,789,52,445};
+        // System.out.print(bruteForce(arr));
+
+        System.out.print(optimal(arr));
+    }
+    public static int bruteForce(int[] arr){ // TC -> O(n * logn)
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    } 
+    public static int optimal(int[] arr){ // TC -> O(n)
+        int max = arr[0];
+        for(int el : arr){
+            max = Math.max(el,max);
+        }
+        return max;
+    }
+}
