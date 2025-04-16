e2a81bf856ea8d87c1172fd7c5c97ea33be7c7fd
pkvstar <pk8649726@gmail.com>
2025-04-16 21:08:51 +0530

    three-greedy

--- Greedy/three.java
+++ Greedy/three.java
@@ -1,8 +1,15 @@
 import java.util.*;
 public class three{   
     // Shortest Job First (or SJF) CPU Scheduling
-    public static void main(String args[]){
-        int[] arr = {5,5,5,5,10,10,20,20};
-        
+    public static void main(String args[]){ // TC -> O( n ) + O(n * logn)
+        int[] arr = {4,3,7,1,2};
+        Arrays.sort(arr);
+        int time = 0;
+        int waitTime = 0;
+        for(int i=0;i<arr.length;i++){
+            waitTime+=time;
+            time+=arr[i];
+        }
+        System.out.print(waitTime/arr.length);
     }
 }
